3cd76a2908c33b27db9e2653da3d1b29f1c9f519
Ahmad Ismail <ahmed.ismail688@gmail.com>
2021-10-29 10:39:50 +0200

    Initial Commit
    Initial commit

---
+++ Repositories/RepositoriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RepositoriesApp.swift
+//  Repositories
+//
+//  Created by Ahmad Ismail on 29/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct RepositoriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
